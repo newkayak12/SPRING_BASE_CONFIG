fd069c6c9a8bf01f3e7e4cd4947b29f12093ea1f
newkayak12 <newkayak12@gmail.com>
2022-08-18 09:48:09 +0900

    configServer

--- src/main/java/com/base/config/ConfigApplication.java
+++ src/main/java/com/base/config/ConfigApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
+//@EnableEurekaServer
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigApplication {
